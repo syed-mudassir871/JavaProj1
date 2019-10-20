f775c24c00d6aa7b30751680f766855ff385e60b
syed-mudassir871 <syedmudassir871@gmail.com>
2019-10-21 03:41:26 +0500

    modified

--- Hello.java
+++ Hello.java
@@ -2,7 +2,9 @@ public class Hello{
   
   public static void main(String[] args) {
    
-      System.out.println("hello world");
+    for(int i=1;i<=10;i++){ 
+      System.out.println("hello world"+i);
+    }
   }
   
 
